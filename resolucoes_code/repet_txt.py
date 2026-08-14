df95505f8dc7bf1cb963b88e4759f0a048007342
Rômulo Sá Leitão <romuloin@gmail.com>
2026-08-14 01:24:38 -0300

    Modify script to repeat input string based on user input
    Updated the script to request a string and an integer for repetition, then displays the repeated string.

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,9 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Solicita uma string e um número inteiro como entrada
+texto = input("Digite uma string: ")
+vezes = int(input("Digite um número inteiro para repetição: "))
+
+# Retorna a string repetida o número de vezes informado (com um espaço para organização)
+resultado = (texto + " ") * vezes
+
+# Exibe o resultado final
+print("O texto repetido é:", resultado)
